6729ceb22b59b553827672c34976bac305b1cf4c
thelockymichael <thelockymichael@gmail.com>
2019-02-27 12:35:44 +0200

    123456

--- 2D Space Shooter/Assets/MenuManager.cs
+++ 2D Space Shooter/Assets/MenuManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 
 public class MenuManager : MonoBehaviour
@@ -10,12 +11,56 @@ public class MenuManager : MonoBehaviour
     public GameObject storyImage;
     private bool openMenu = false;
 
+    public GameObject settingsMenu;
 
+    public GameObject mainMenu;
 
+    public EventSystem myEventSystem;
 
+    public EventSystem optionsEventSystem;
 
+    IEnumerator optionsDelay()
+    {
+        myEventSystem.enabled = false;
+        optionsEventSystem.enabled = true;
+        yield return new WaitForSeconds(0.2f);
+        settingsMenu.SetActive(true);
+        mainMenu.SetActive(false);
+        StartCoroutine(highlightGameOverBtn());
+
+       // Time.timeScale = 0.0f;
+    }
+
+    IEnumerator mainMenuDelay()
+    {
+        myEventSystem.enabled = true;
+        optionsEventSystem.enabled = false;
+        yield return new WaitForSeconds(0.2f);
+        settingsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+        StartCoroutine(highlightBtn());
+
+        // Time.timeScale = 0.0f;
+    }
+
+    IEnumerator highlightGameOverBtn()
+    {
+        optionsEventSystem.SetSelectedGameObject(null);
+        yield return null;
+        optionsEventSystem.SetSelectedGameObject(optionsEventSystem.firstSelectedGameObject);
+    }
+
+    IEnumerator highlightBtn()
+    {
+        myEventSystem.SetSelectedGameObject(null);
+        yield return null;
+        myEventSystem.SetSelectedGameObject(myEventSystem.firstSelectedGameObject);
+    }
     void Start()
     {
+        settingsMenu.SetActive(false);
+        optionsEventSystem.enabled = false;
+
         //anim = GetComponent<Animator>();
         storyImage.SetActive(false);
        // UIFaderController = GetComponent<UIFader>();
@@ -25,20 +70,43 @@ public class MenuManager : MonoBehaviour
 
     public void Play()
     {
-
         //SceneManager.LoadScene("game");
     }
     public void Story()
     {
         storyImage.SetActive(false);
+        settingsMenu.SetActive(false);
 
         //SceneManager.LoadScene("game");
+    }
+
+    public void Options()
+    {
+        StartCoroutine(optionsDelay());
+       // settingsMenu.SetActive(true);
+      //  mainMenu.SetActive(false);
+
     }
 
     public void QuitToDesktop()
     {
         Application.Quit();
         //SceneManager.LoadScene("game");
+    }
+
+    public void ResetHiScore()
+    {
+        PlayerPrefs.DeleteKey("HighScore");
+
+    }
+
+    public void ReturnToMainMenu()
+    {
+        StartCoroutine(mainMenuDelay());
+
+        //  mainMenu.SetActive(true);
+        //  settingsMenu.SetActive(false);
+        // storyImage.SetActive(false);
     }
 
     public void Pause()
